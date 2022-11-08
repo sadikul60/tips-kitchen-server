afdf379ac823b2b1fcbde9d53fad38dd9773ad00
mdsadikul <mdsadikulislam891@gamil.com>
2022-11-08 09:26:46 +0600

    Added get services data from mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
 
@@ -8,6 +9,32 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@cluster0.8jrtwg1.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log(uri)
+
+async function run() {
+    try{
+        const serviceCollection = client.db('serviceReview').collection('services');
+
+        app.get('/services', async(req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(err => console.error(err));
+
 
 app.get('/', (req, res) => {
     res.send('Service Review Server is Running.');
